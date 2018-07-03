56d3e4c15c0dcdae3f1ac26d4e953499caebb623
poptart-sommelier <rich.d.nospam@gmail.com>
2018-07-02 22:33:48 -0400

    fix

--- visual_traceroute.py
+++ visual_traceroute.py
@@ -5,4 +5,4 @@ from scapy.all import *
 # can also provide a list of targets [''],['']
 result, unanswered = traceroute(['www.microsoft.com'])
 
-res.graph(target="> /tmp/graph.csv")
+res.graph(target="> /tmp/graph.svg")
